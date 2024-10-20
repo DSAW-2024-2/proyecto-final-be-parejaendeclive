e9271f3b733dacb264db436764e279192c3cf8b2
AndreSosa21 <andrejulieth22@gmail.com>
2024-10-19 20:07:46 -0500

    users validations

--- functions/api_routes/users.js
+++ functions/api_routes/users.js
@@ -10,23 +10,65 @@ admin.initializeApp(
 );
 
 const dataBase = admin.firestore();
+function string_validation( name, LastName){
+    let data =[name,LastName]
+    const letters = /^[A-Za-z\s]+$/;
+    return data.every(item => letters.test(item));
+    
+}
+
 
 route_users.get('/', (req, res) => {
     return res.status(200).json({message: 'users succesfull!'});
 });
 
 route_users.post('/',async (req,res) =>{
-    await dataBase.collection('users').doc('/' + req.body.id + '/').create(
-        {
-            name: req.body.name,
-            LastName: req.body.LastName,
-            email: req.body.email,
-            number: req.body.number,
-            password: req.body.password
-
+    try{
+        const {id,name,LastName, email, number, password}= req.body;
+        if (!id || !name || !LastName || !email || !number ||!password) {
+            return res.status(400).json({ message: "incomplete user data" });
         }
-    )
-    return res.status(201).json({ message: "User added" });
+        
+        const users = dataBase.collection('users').doc(id);
+        const doc = await users.get();
+        if (doc.exists) {
+            return res.status(400).json({ message: "ID already in use" });
+        }
+        
+        if (!/^\d+$/.test(id)) {
+            return res.status(400).json({ message: "incorrect type of data: ID must be ONLY numbers" });
+        }
+        if (!/^\d+$/.test(number)) {
+            return res.status(400).json({ message: "incorrect type of data: number must contain ONLY numbers" });
+        }
+        if(!string_validation(name,LastName)){
+            return res.status(400).json({ message: "incorrect type of data: name or LastName must be ONLY string" });
+        }
+        if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+            return res.status(400).json({ message: "email must have a valid format" });
+        }
+        if (password.length < 8) {
+            return res.status(400).json({ message: "Password must be at least 8 characters long" });
+        }
+        
+        await dataBase.collection('users').doc('/' + req.body.id + '/').create(
+            {
+                name: req.body.name,
+                LastName: req.body.LastName,
+                email: req.body.email,
+                number: req.body.number,
+                password: req.body.password
+    
+            }
+        )
+        return res.status(201).json({ message: "User added" });
+
+
+    }
+    catch (error) {
+        res.status(500).json({ message: "User not added", error: error.message });
+    }
+    
 })
 
 module.exports = route_users;
--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,12 @@
+//database functions
 const functions = require("firebase-functions");
-const express = require('express');
 const logger = require("firebase-functions/logger");
 const { databaseURL } = require("firebase-functions/params");
+
+const express = require('express');
 const route_users= require('./api_routes/users');
-
-
-
 const app = express();
+
 app.use('/users', route_users);
 
 exports.app = functions.https.onRequest(app);
