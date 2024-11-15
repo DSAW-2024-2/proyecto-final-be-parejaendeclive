4a1e557a724c822a8d888da77fe37e296b8d02c9
AndreSosa21 <andrejulieth22@gmail.com>
2024-11-14 21:40:14 -0500

    domain front

--- functions/index.js
+++ functions/index.js
@@ -17,7 +17,7 @@ const route_roles = require("./api_routes/roles");
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-    origin: ['http://localhost:5173'], // Agrega ambos dominios
+    origin: ['https://proyecto-final-be-parejaendeclive.vercel.app','https://proyecto-final-fe-parejaendeclive.vercel.app'], // Agrega ambos dominios
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
